295e434f41595f58fb9597246ed00c0191f30a98
Shannon Woods <smwoods4@gmail.com>
2020-11-04 12:51:32 -0500

    breaking for lunch

--- tenmo-client/src/main/java/com/techelevator/tenmo/App.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/App.java
@@ -68,6 +68,8 @@ private static final String API_BASE_URL = "http://localhost:8080/";
 	}
 
 	private void viewCurrentBalance() {
+		// TODO Auto-generated method stub
+
 	}
 
 	private void viewTransferHistory() {
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountSqlDAO.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountSqlDAO.java
@@ -1,5 +1,7 @@
 package com.techelevator.tenmo.dao;
 
+import java.util.List;
+
 public class AccountSqlDAO implements AccountDAO {
 
 	@Override
@@ -18,6 +20,18 @@ public class AccountSqlDAO implements AccountDAO {
 	public int increaseBalance(int amount) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public List<Transaction> listAllTransfers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Transaction getDetails(int transactionId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
