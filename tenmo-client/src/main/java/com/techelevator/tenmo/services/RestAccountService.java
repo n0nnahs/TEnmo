93a613e9aea1d89cbe6d8c06c827ecf74b109974
Jesse Walls <drummerjw505@yahoo.com>
2020-11-04 17:41:11 -0500

    deleted interface class|added getAccountBalance() method to AccountService class,created Accountexception class

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/RestAccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/RestAccountService.java
@@ -10,58 +10,29 @@ import org.springframework.http.MediaType;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+import com.techelevator.tenmo.models.Account;
 
-public class RestAccountService implements UserService {
+public class RestAccountService  {
 	public static String AUTH_TOKEN = "";
 	private final String BASE_URL;
 	private final RestTemplate restTemplate = new RestTemplate();
-
+	
 	
 	public RestAccountService(String url) {
 		this.BASE_URL = url;
 	}
-
-	@Override
-	public User getAccountBalance(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Account getAccountBalance(int id) throws AccountServiceException {
+		Account balance = null;
+		try {
+			balance = restTemplate.exchange(BASE_URL + "accounts/balance", HttpMethod.GET, makeAuthEntity(), Account.class).getBody();
+		} catch (RestClientResponseException ex) {
+			throw new AccountServiceException(ex.getRawStatusCode() + " : " + ex.getResponseBodyAsString());
+		}
+		return balance;
 	}
 
-	@Override
-	public User sendBucks() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User requestBucks() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User viewTransferHistory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User viewPendingTransfers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User listTransactionsForAccount() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User getTransactionsDetails() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 	  private HttpEntity makeAuthEntity() {
 		    HttpHeaders headers = new HttpHeaders();
 		    headers.setBearerAuth(AUTH_TOKEN);
